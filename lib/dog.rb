e3b4e946062ef759d25a5c43377402274089d68c
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-03 17:49:29 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+
+
+class Dog
+@@all = []
+attr_accessor :name
+
+def initialize(name)
+    @name = name
+    save
+end
+
+
+
+def self.all
+    @@all
+end
+
+def self.print_all
+   puts @@all.map {|dog| dog.name}
+end
+
+def self.clear_all
+    puts "#{@@all.clear}"
+end
+
+def save 
+    @@all << self
+end
+
+end
+
+
+
+
+
